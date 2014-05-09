efe5393c8ebb4c7aba95c338c71f12ad7049ee8b
Jean-Baptiste Mouret <mouret@isir.upmc.fr>
2014-05-09 14:41:20 +0200

    more parameters

--- svn_diff_prev.sh
+++ svn_diff_prev.sh
@@ -5,4 +5,5 @@ echo "shift \$((\$# - 2))" >> /tmp/cwdiff
 echo "wdiff -n \$1 \$2|colordiff" >>/tmp/cwdiff
 chmod u+x /tmp/cwdiff
 
-exec svn diff -rPREV --diff-cmd "/tmp/cwdiff" "$@"|less -R
+
+exec svn diff -r$1 --diff-cmd "/tmp/cwdiff" $2|less -R
