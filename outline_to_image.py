3ca999952e85981a6bb982ebfe3a765a00fdfeea
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2019-05-21 16:49:06 +0200

    add check_avx

--- outline_to_image.py
+++ outline_to_image.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3
 import sys
 # see http://pillow.readthedocs.io/en/4.1.x/handbook/tutorial.html
 from PIL import Image
@@ -23,6 +23,7 @@ font_bold_big = ImageFont.truetype("Arial Bold.ttf", int(font_size*1))
 
 
 draw = ImageDraw.Draw(image)
+draw.rectangle((0, 0, size_x, size_y), fill='#000000', outline=None)
 
 k = 200
 x = 100
@@ -32,12 +33,12 @@ for f in sys.argv:
             draw.text((x, k), strip_stars(i), (0,0,0), font=font_bold)
             k += font_size * 1.2
         elif len(i) > 1 and i[0] == '*':
-            draw.rectangle((x, k, x + 400, k + font_size * 1.3), fill='#9EBCD2', outline=None)
+            draw.rectangle((x, k, x + 400, k + font_size * 1.3), fill='#222222', outline=None)
             draw.line((x, k, x + 400, k), fill='#042037', width=3)
-            draw.text((x, k), strip_stars(i), (0,0,0), font=font_bold_big)
+            draw.text((x, k), strip_stars(i), fill='#ffffff',font=font_bold_big)
             k += font_size * 1.4
         else:
-            draw.text((x, k), i, (0,0,0), font=font)
+            draw.text((x, k), i, font=font,fill='#ffffff')
             k += font_size * 1.2
         if k > size_y * 0.75:
             x += 500
